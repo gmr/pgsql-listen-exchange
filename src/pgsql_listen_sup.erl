e7ab0b6a8b61aa5a09fdcd78ce0a39ac7abab494
Gavin M. Roy <gavinmroy@gmail.com>
2014-04-14 00:23:00 -0400

    Add comments, reorder functions, consolidate exports

--- src/pgsql_listen_sup.erl
+++ src/pgsql_listen_sup.erl
@@ -1,3 +1,12 @@
+%%==============================================================================
+%% @author Gavin M. Roy <gavinr@aweber.com>
+%% @copyright 2014 AWeber Communications
+%% @end
+%%==============================================================================
+
+%% @doc Supervisor for the PgSQL LISTEN Exchange worker
+%% @end
+
 -module(pgsql_listen_sup).
 
 -behaviour(mirrored_supervisor).
@@ -10,19 +19,18 @@
                     {requires,    kernel_ready},
                     {enables,     pgsql_listen_exchange}]}).
 
--export([init/1]).
--export([start_link/0]).
+-export([init/1, start_link/0]).
 
 -include_lib("rabbit_common/include/rabbit.hrl").
-
-init([]) ->
-    {ok, {{one_for_one, 3, 10},
-          [{?WORKER,
-           {?WORKER, start_link, []},
-           permanent, ?MAX_WAIT, worker, [?WORKER]}]}}.
 
 start_link() ->
      mirrored_supervisor:start_link(
        {local, ?MODULE}, ?MODULE,
        fun rabbit_misc:execute_mnesia_transaction/1,
        ?MODULE, []).
+
+init([]) ->
+    {ok, {{one_for_one, 3, 10},
+          [{?WORKER,
+           {?WORKER, start_link, []},
+           permanent, ?MAX_WAIT, worker, [?WORKER]}]}}.
