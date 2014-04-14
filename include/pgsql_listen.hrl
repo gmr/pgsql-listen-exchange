b12c526a5cdb3aeaca1ba5a715ba7c4b41683352
Gavin M. Roy <gavinmroy@gmail.com>
2014-04-14 00:20:55 -0400

    Add records, update constant name for password

--- include/pgsql_listen.hrl
+++ include/pgsql_listen.hrl
@@ -1,4 +1,11 @@
+%%==============================================================================
+%% @author Gavin M. Roy <gavinr@aweber.com>
+%% @copyright 2014 AWeber Communications
+%% @end
+%%==============================================================================
 
+%% @doc Constantsand records for the pgsql-listen exchange
+%% @end
 -define(X_TYPE, <<"x-pgsql-listen">>).
 -define(X_DESC, <<"PostgreSQL LISTEN Exchange.">>).
 
@@ -8,5 +15,10 @@
 -define(DEFAULT_HOST, <<"localhost">>).
 -define(DEFAULT_PORT, 5432).
 -define(DEFAULT_USER, <<"postgres">>).
--define(DEFAULT_PASS, <<"">>).
+-define(DEFAULT_PASSWORD, <<"">>).
 -define(DEFAULT_DBNAME, <<"postgres">>).
+
+-record(pgsql_listen_dsn, {host, port, user, password, dbname}).
+-record(pgsql_listen_state, {pgsql, vhosts}).
+-record(pgsql_listen_vhost, {connection, channel, bindings}).
+-record(pgsql_listen_binding, {exchange, key}).
