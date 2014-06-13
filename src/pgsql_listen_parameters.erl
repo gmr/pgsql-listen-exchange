574e45020b4db4280b7f9aa518211c8c394ecdfa
Simon MacMullen <simon@rabbitmq.com>
2014-06-13 13:05:46 +0100

    Remove unneeded code about runtime parameters.

--- src/pgsql_listen_parameters.erl
+++ src/pgsql_listen_parameters.erl
@@ -9,13 +9,9 @@
 
 -module(pgsql_listen_parameters).
 
--behaviour(rabbit_runtime_parameter).
 -behaviour(rabbit_policy_validator).
 
 -export([register/0,
-         notify/4,
-         notify_clear/3,
-         validate/4,
          validate_policy/1]).
 
 -rabbit_boot_step({?MODULE,
@@ -26,29 +22,12 @@
 
 register() ->
   [rabbit_registry:register(Class, Name, ?MODULE) ||
-      {Class, Name} <- [{runtime_parameter, <<"pgsql-listen-host">>},
-                        {runtime_parameter, <<"pgsql-listen-port">>},
-                        {runtime_parameter, <<"pgsql-listen-dbname">>},
-                        {runtime_parameter, <<"pgsql-listen-user">>},
-                        {runtime_parameter, <<"pgsql-listen-password">>},
-                        {policy_validator,  <<"pgsql-listen-host">>},
+      {Class, Name} <- [{policy_validator,  <<"pgsql-listen-host">>},
                         {policy_validator,  <<"pgsql-listen-port">>},
                         {policy_validator,  <<"pgsql-listen-dbname">>},
                         {policy_validator,  <<"pgsql-listen-user">>},
                         {policy_validator,  <<"pgsql-listen-password">>}]],
   ok.
-
-notify(VHost, What, Name, Term) ->
-  rabbit_log:info("notify: ~s,~s,~s,~s~n", [VHost, What, Name, Term]),
-  rabbit_policy:notify(VHost, What, Name, Term).
-
-notify_clear(VHost, What, Name) ->
-  rabbit_log:info("notify_clear: ~s,~s,~s~n", [VHost, What, Name]),
-  rabbit_policy:notify_clear(VHost, What, Name).
-
-validate(VHost, What, Name, Term) ->
-  rabbit_log:info("Validating ~s,~s,~s,~s~n", [VHost, What,Name, Term]),
-  rabbit_policy:validate(VHost, <<"policy">>, Name, Term).
 
 validate_policy(KeyList) ->
   Host     = proplists:get_value(<<"pgsql-listen-host">>, KeyList, none),
