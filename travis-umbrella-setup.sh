a7d785bbcc925dfd7090b611b90934e87111087c
Gavin M. Roy <gavinr@aweber.com>
2015-07-16 23:52:55 -0400

    Update the test/build config/script

--- travis-umbrella-setup.sh
+++ travis-umbrella-setup.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 BUILD_DIR=${BUILD_DIR:-${HOME}/rabbitmq-public-umbrella}
-RABBIT_TAG=${RABBIT_TAG:-v3_5_1}
+RABBIT_TAG=${RABBIT_TAG:-v3_5_3}
 echo "Build dir: ${BUILD_DIR}"
 echo "Travis build dir: ${TRAVIS_BUILD_DIR}"
-if [ ! -d $BUILD_DIR ]; then
-    git clone https://github.com/rabbitmq/rabbitmq-public-umbrella.git $BUILD_DIR
-    cd $BUILD_DIR
-    make co
+
+if [ -d $BUILD_DIR ]; then
+    rm -rf $BUILD_DIR
 fi
+git clone https://github.com/rabbitmq/rabbitmq-public-umbrella.git $BUILD_DIR
 cd $BUILD_DIR
-make BRANCH=rabbitmq_${RABBIT_TAG} up_c
-if [ ! -d "$BUILD_DIR/epgsql-wrapper" ]; then
-    git clone https://github.com/gmr/epgsql-wrapper.git
-fi
-rm -rf ${BUILD_DIR}/rabbitmq-public-umbrella
+git checkout -b rabbitmq_${RABBIT_TAG}
+git pull origin rabbitmq_${RABBIT_TAG}
+make co
+git clone https://github.com/gmr/epgsql-wrapper.git
 cp -r ${TRAVIS_BUILD_DIR} ${BUILD_DIR}/
